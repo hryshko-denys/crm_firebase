3a3079f9fde74b13170eb6eda5306c60f671fb18
Denys <gryshko_denys@gmail.com>
2020-09-03 17:40:23 +0300

    registration with firebase

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,3 +1,6 @@
 export default {
   logout: 'Вы вышли из системы',
+  'auth/user-not-found': 'Пользователя с такими данными не найдено',
+  'auth/wrong-password': 'Пароль не верный',
+  'auth/email-already-in-use': 'Email уже занят',
 };
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -89,6 +89,7 @@
 </template>
 
 <script>
+/* eslint-disable */
 import { email, required, minLength } from 'vuelidate/lib/validators';
 
 export default {
@@ -106,7 +107,7 @@ export default {
     agree: { checked: (v) => v },
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch();
         return;
@@ -118,9 +119,10 @@ export default {
         name: this.name,
       };
 
-      console.log(formData);
-
-      this.$router.push('/');
+      try {
+        await this.$store.dispatch('register', formData)
+        this.$router.push('/')
+      } catch (e) {}
     },
   },
 };
